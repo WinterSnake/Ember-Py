cabc47e910fce91ff4f3e97a717bc231b74ede55
Winter_Snake <blizzardsnake98@gmail.com>
2024-11-27 07:06:01 -0500

    add: input file cli parse
    add: EBNF file
    refactor: test-00

--- emberc/__main__.py
+++ emberc/__main__.py
@@ -9,39 +9,25 @@ import sys
 from pathlib import Path
 
 from .frontend import Lexer, Parser
-from .middleware.nodes import NodeFunctionDefinition, NodeBinaryExpression
+
 
 ## Constants
+SRC: Path = None  #type: ignore
 
 
 ## Functions
-def visit_node(node) -> int | None:
-    if isinstance(node, NodeFunctionDefinition):
-        print(f"Function[name:{node.name}, return:{node.return_type}]")
-        for i, statement in enumerate(node.body):
-            print(f"[{i}] = {visit_node(statement)}")
-    elif isinstance(node, NodeBinaryExpression):
-        lhs = visit_node(node.lhs)
-        rhs = visit_node(node.rhs)
-        assert lhs is not None and rhs is not None
-        match node.type:
-            case NodeBinaryExpression.Type.Add:
-                return lhs + rhs
-            case NodeBinaryExpression.Type.Sub:
-                return lhs - rhs
-            case NodeBinaryExpression.Type.Mul:
-                return lhs * rhs
-            case NodeBinaryExpression.Type.Div:
-                return lhs // rhs
-            case NodeBinaryExpression.Type.Mod:
-                return lhs % rhs
-    else:
-        return node.value
-    return None
 
 
 ## Body
-lexer = Lexer("./tests/test-00.ember")
+if len(sys.argv) < 2:
+    print(f"No input file found. Usage: {sys.argv[0]} <file.ember>", file=sys.stderr)
+    sys.exit(1)
+SRC = Path(sys.argv[1])
+if not SRC.exists():
+    print(f"'{SRC}' is not a valid file. Usage: {sys.argv[0]} <file.ember>", file=sys.stderr)
+    sys.exit(1)
+lexer = Lexer(SRC)
 parser = Parser(lexer.lex())
 ast = parser.parse()
-visit_node(ast)
+for node in ast:
+    print(node)
