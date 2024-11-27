071825bb985d7d8a9b4c8180d626071de0b063cf
Winter_Snake <blizzardsnake98@gmail.com>
2024-11-26 19:58:52 -0500

    refactor: state machine

--- emberc/frontend/lexer.py
+++ emberc/frontend/lexer.py
@@ -28,7 +28,7 @@ STATES: tuple[str, ...] = (
 )
 
 ## Functions
-def _get_symbol_type(char: str) -> Token.Type:
+def _get_symbol_type(char: str) -> Token.Type | None:
     """
     """
 
@@ -56,16 +56,19 @@ def _get_symbol_type(char: str) -> Token.Type:
             return Token.Type.SymbolFSlash
         case '%':
             return Token.Type.SymbolPercent
-    assert False, "Unreachable"
+        case '_':
+            return None
 
-def _get_word_type(word: str) -> Token.Type:
+
+def _get_word_data(buffer: str) -> tuple[Token.Type, str | None]:
     """
     """
-    return {
+
+    _type = {
         'fn': Token.Type.KeywordFunction,
-        # -Types
-        'void': Token.Type.TypeVoid,
-    }.get(word, Token.Type.Identifier)
+    }.get(buffer, Token.Type.Identifier)
+    value = buffer if _type is Token.Type.Identifier else None
+    return (_type, value)
 
 
 ## Classes
@@ -82,90 +85,138 @@ class Lexer:
         self.column: int = 0
         self.offset: int = 0
         self._fp: TextIO | None = None
-        self.state: Lexer.State = Lexer.State.Default
 
     # -Instance Methods
+    # --Lex
     def lex(self) -> Generator[Token, None, None]:
+        '''
+        '''
         self._fp = self.file.open('r')
         buffer: str = ""
         token_position: tuple[int, int, int] | None = None
+        state_current: Lexer.State = Lexer.State.Default
+        state_new: Lexer.State = state_current
         while c := self._advance():
-            # -Whitespace
-            if c.isspace():
-                match self.state:
-                    # --State[Word]
-                    case Lexer.State.Word:
-                        assert token_position is not None
-                        _type = _get_word_type(buffer)
-                        value: str | None = None
-                        if _type is Token.Type.Identifier:
-                            value = buffer
-                        yield Token(self.file, token_position, _type, value)
-                    # --State[Number]
-                    case Lexer.State.Number:
-                        assert token_position is not None
-                        yield Token(self.file, token_position, Token.Type.NumberLiteral, buffer)
-                self.state = Lexer.State.Default
-                buffer = ""
-                token_position = None
-            # -Word
-            elif c.isalpha() or c == '_':
-                match self.state:
-                    # --State[Default]
-                    case Lexer.State.Default:
-                        self.state = Lexer.State.Word
+            match state_current:
+                # -State[Default]
+                case Lexer.State.Default:
+                    # --Char[Whitespace]
+                    if c.isspace():
+                        pass
+                    # --Char[Symbol]
+                    elif c in SYMBOLS:
+                        state_new = Lexer.State.Symbol
                         token_position = self.position
                         buffer = c
-                    # --State[Word]
-                    case Lexer.State.Word:
-                        buffer += c
-                    # --State[Number]
-                    case Lexer.State.Number:
-                        assert False, "Unreachable"
-            # -Number
-            elif c.isdigit():
-                match self.state:
-                    # --State[Default]
-                    case Lexer.State.Default:
-                        self.state = Lexer.State.Number
+                    # --Char[Number]
+                    elif c.isdigit():
+                        state_new = Lexer.State.Number
                         token_position = self.position
                         buffer = c
-                    # --State[Word]
-                    case Lexer.State.Word:
-                        assert False, "Unreachable"
-                    # --State[Number]
-                    case Lexer.State.Number:
-                        buffer += c
-            # -Symbol
-            elif c in SYMBOLS:
-                match self.state:
-                    # --State[Word]
-                    case Lexer.State.Word:
+                    # --Char[Word]
+                    elif c.isalpha() or c == '_':
+                        state_new = Lexer.State.Word
+                        token_position = self.position
+                        buffer = c
+                # -State[Symbol]
+                case Lexer.State.Symbol:
+                    # --Char[Whitespace]
+                    if c.isspace():
                         assert token_position is not None
-                        _type = _get_word_type(buffer)
-                        value: str | None = None
-                        if _type is Token.Type.Identifier:
-                            value = buffer
-                        yield Token(self.file, token_position, _type, value)
-                        self.state = Lexer.State.Default
-                        self.buffer = ""
-                    # --State[Number]
-                    case Lexer.State.Number:
+                        state_new = Lexer.State.Default
+                        token = Token(self.file, token_position, _get_symbol_type(buffer), None)
+                        token_position = None
+                        buffer = ""
+                        yield token
+                    # --Char[Symbol]
+                    elif c in SYMBOLS:
+                        if _get_symbol_type(buffer + c) is not None:
+                            buffer += c
+                        else:
+                            assert token_position is not None
+                            token = Token(self.file, token_position, _get_symbol_type(buffer), None)
+                            token_position = self.position
+                            buffer = c
+                            yield token
+                    # --Char[Number]
+                    elif c.isdigit():
                         assert token_position is not None
-                        yield Token(self.file, token_position, Token.Type.NumberLiteral, buffer)
-                        self.state = Lexer.State.Default
-                        self.buffer = ""
-                # --State[Default]
-                _type = _get_symbol_type(c)
-                yield Token(self.file, self.position, _type, None)
+                        state_new = Lexer.State.Number
+                        token = Token(self.file, token_position, _get_symbol_type(buffer), None)
+                        token_position = self.position
+                        buffer = c
+                        yield token
+                    # --Char[Word]
+                    elif c.isalpha():
+                        assert token_position is not None
+                        state_new = Lexer.State.Word
+                        token = Token(self.file, token_position, _get_symbol_type(buffer), None)
+                        token_position = self.position
+                        buffer = c
+                        yield token
+                # -State[Number]
+                case Lexer.State.Number:
+                    # --Char[Whitespace]
+                    if c.isspace():
+                        assert token_position is not None
+                        state_new = Lexer.State.Default
+                        token = Token(self.file, token_position, Token.Type.NumberLiteral, buffer)
+                        token_position = None
+                        buffer = ""
+                        yield token
+                    # --Char[Symbol]
+                    elif c in SYMBOLS:
+                        assert token_position is not None
+                        state_new = Lexer.State.Symbol
+                        token = Token(self.file, token_position, Token.Type.NumberLiteral, buffer)
+                        token_position = self.position
+                        buffer = c
+                        yield token
+                    # --Char[Number]
+                    elif c.isdigit():
+                        buffer +=  c
+                    # --Char[Word]
+                    elif c.isalpha():
+                        assert token_position is not None
+                        state_new = Lexer.State.Word
+                        token = Token(self.file, token_position, Token.Type.NumberLiteral, buffer)
+                        token_position = self.position
+                        buffer = c
+                        yield token
+                # -State[Word]
+                case Lexer.State.Word:
+                    # --Char[Whitespace]
+                    if c.isspace():
+                        assert token_position is not None
+                        state_new = Lexer.State.Default
+                        _type, value = _get_word_data(buffer)
+                        token = Token(self.file, token_position, _type, value)
+                        token_position = None
+                        buffer = ""
+                        yield token
+                    # --Char[Symbol]
+                    elif c in SYMBOLS:
+                        assert token_position is not None
+                        state_new = Lexer.State.Symbol
+                        _type, value = _get_word_data(buffer)
+                        token = Token(self.file, token_position, _type, value)
+                        token_position = self.position
+                        buffer = c
+                        yield token
+                    # --Char[Number | Word | '_']
+                    elif c.isalnum() or c == '_':
+                        buffer +=  c
+            state_current = state_new
         self._fp.close()
 
-    # -Instance Methods: Private
+    # --Control
     def _advance(self) -> str | None:
+        '''
+        '''
         assert self._fp is not None
         if self._fp.closed:
             return None
-        char: str = self._fp.read(1)
+        char = self._fp.read(1)
         if char is None:
             return None
         if char == '\n':
@@ -186,5 +237,6 @@ class Lexer:
         '''
         '''
         Default = auto()
-        Word = auto()
+        Symbol = auto()
         Number = auto()
+        Word = auto()
